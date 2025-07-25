6749ba031759c350a596c8b3d8fb9343d0492336
kvng-dhay1 <dhayxnxx@gmail.com>
2025-07-25 21:25:16 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;OAFmxK6L#VbZ3zqGD3hhyoTDpz3yB9_Mv6nOyA4Pmk0lNx95GxPc", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "2347045702771", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
